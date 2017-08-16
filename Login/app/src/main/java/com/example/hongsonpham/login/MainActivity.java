fcb0e8174425d3cb6c5ef80e4091f9d8c72b6c26
SonPH <hongsongp97@gmail.com>
2017-08-16 13:26:06 +0700

    Add First-Greeting Project

--- Login/app/src/main/java/com/example/hongsonpham/login/MainActivity.java
+++ Login/app/src/main/java/com/example/hongsonpham/login/MainActivity.java
@@ -1,40 +1,25 @@
 package com.example.hongsonpham.login;
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
 import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.internal.Utility;
 import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import com.squareup.picasso.Picasso;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -60,67 +45,90 @@ public class MainActivity extends AppCompatActivity {
         facebookId = (TextView) findViewById(R.id.facebookId);
         email = (TextView) findViewById(R.id.email);
         name = (TextView) findViewById(R.id.name);
-        image = (ImageView) findViewById(R.id.image);
+        image = (ImageView) findViewById(R.id.imageView);
 
         processLogin();
     }
 
     public void processLogin() {
-        if (accessToken != null) {
-            accessToken = com.facebook.AccessToken.getCurrentAccessToken();
-            LoginManager.getInstance().logOut();
-        }
-        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                //check for granted permission
-                System.out.println("Granted permission::" + loginResult.getRecentlyGrantedPermissions());
-                //create a graph request to get the user detail
-                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback()
-                {
+
+//        if (accessToken != null) {
+//            accessToken = com.facebook.AccessToken.getCurrentAccessToken();
+//            System.out.println("logined");
+//            LoginManager.getInstance().logOut();
+//        }
+        Bundle params = new Bundle();
+        params.putString("fields", "id,name,email,picture.type(large),cover");
+        GraphRequestAsyncTask graphRequestAsyncTask = new GraphRequest(AccessToken.getCurrentAccessToken(), "me", params, HttpMethod.GET,
+                new GraphRequest.Callback() {
                     @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        String userDetail = response.getRawResponse();
-                        try {
-                            JSONObject jsonObject = new JSONObject(userDetail);
-                            System.out.println("jsonObject::" + jsonObject);
-                            String facebookIdText = jsonObject.getString("id");
-                            facebookId.setText(facebookIdText);
-                            String nameText = jsonObject.getString("name");
-                            name.setText(nameText);
-                            String emailText = jsonObject.getString("email");
-                            email.setText(emailText);
-                            String facebookImage = "https://graph.facebook.com/" + facebookId + "/picture?type=large";
-//                            Glide.with(getApplicationContext()).load(facebookImage).into(image);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                    public void onCompleted(GraphResponse response) {
+                        if (response != null) {
+                            System.out.println(response);
+                            String userDetail = response.getRawResponse();
+                            try {
+                                JSONObject jsonObject = new JSONObject(userDetail);
+                                System.out.println("jsonObject::" + jsonObject);
+                                String facebookIdText = jsonObject.getString("id");
+                                facebookId.setText(facebookIdText);
+                                String nameText = jsonObject.getString("name");
+                                name.setText(nameText);
+//                                String emailText = jsonObject.getString("email");
+//                                email.setText(emailText);
+                                String facebookImage = "https://graph.facebook.com/" + facebookIdText + "/picture?type=large";
+                                System.out.println(facebookImage);
+//                                String facebookCover = "https://graph.facebook.com/" + facebookIdText + "/photos";
+//                                System.out.println(facebookCover);
+//                                Picasso.with(getApplicationContext()).load(facebookCover).into(image);
+
+                                String finalCoverPhoto = "";
+                                if (jsonObject.has("cover")) {
+                                    String getInitialCover = jsonObject.getString("cover");
+
+                                    if (getInitialCover.equals("null")) {
+                                        jsonObject = null;
+                                    } else {
+                                        JSONObject JOCover = jsonObject.optJSONObject("cover");
+
+                                        if (JOCover.has("source")) {
+                                            finalCoverPhoto = JOCover.getString("source");
+                                        } else {
+                                            finalCoverPhoto = null;
+                                        }
+                                    }
+                                } else {
+                                    finalCoverPhoto = null;
+                                }
+                                System.out.println(finalCoverPhoto);
+                                Picasso.with(getApplicationContext()).load(finalCoverPhoto).into(image);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
-                });
+                }).
 
-//                Bundle parameter = new Bundle();
-//                parameter.putString("fields", "name, email");
-//                graphRequest.setParameters(parameter);
-                graphRequest.executeAsync();
-            }
+                executeAsync();
 
-            @Override
-            public void onCancel() {
-                System.out.println("Login Cancel");
-            }
+        new
 
-            @Override
-            public void onError(FacebookException error) {
-                System.out.println("Network error");
-            }
-        });
+                GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "https://graph.facebook.com/1136561069821009?fields={fieldname_of_type_CoverPhoto}",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback()
 
-//        loginButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile, email"));
-//            }
-//        });
+                {
+                    public void onCompleted(GraphResponse response) {
+            /* handle the result */
+                    }
+                }
+        ).
+
+                executeAsync();
     }
 
 //    public String getHashKey() {
